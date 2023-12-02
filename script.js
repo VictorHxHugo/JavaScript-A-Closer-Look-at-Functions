f2764dd128914c31b6000ae4645b83c399c35943
VictorHxHugo <victorzk6@hotmail.com>
2023-12-02 13:35:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const bookings = [];
+
+const creatBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+creatBooking('LH123');
+creatBooking('LH123', 2, 800);
+creatBooking('LH123', 7);
+creatBooking('LH123', 5);
+creatBooking('LH123', undefined, 1000);
