600db0467cb95f7eb3fd26cd0fbe6a1227626e1a
VictorHxHugo <victorzk6@hotmail.com>
2023-12-03 17:04:28 -0300

    Functions Accepting Callback Functions

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ creatBooking('LH123', undefined, 1000);
 
 ///////////////////////
 //  How Passing Arguments Works: Value vs. Reference
-
+/*
 const flight = 'LH234';
 const victor = {
   name: 'Victor Hugo',
@@ -56,3 +56,33 @@ const newPassport = function (person) {
 newPassport(victor);
 checkIn(flight, victor);
 console.log(victor);
+*/
+
+///////////////////////
+// Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('🖐');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martin', 'Adam'].forEach(high5);
